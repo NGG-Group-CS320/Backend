c6303ee2cb81f305c344d81ce700d5c37fd47b16
AlexLamson <alex.lamson@gmail.com>
2016-12-09 21:21:27 -0500

    parsed query row to variables

--- src/query_db.py
+++ src/query_db.py
@@ -2,7 +2,16 @@
 #
 # Small script to show PostgreSQL and Pyscopg together
 #
+import sys
 import psycopg2
+
+
+# parses query results into variables to be used to compute health score
+def parse_health_inputs_row(row):
+	systemid, timestamp, writes, reads, cpu, bandwidth, delayedAcks = row[0], row[1], row[2:2+21], row[2+21:2+21+21], row[-3], row[-2], row[-1]
+	writes = [float(x) for x in writes]
+	reads = [float(x) for x in reads]
+	return systemid, timestamp, writes, reads, cpu, bandwidth, delayedAcks
 
 
 def main():
@@ -18,7 +27,7 @@ def main():
 		# cur.execute("""
 		# 	SELECT *
 		# 	from hp
-		# 	LIMIT 10
+		# 	LIMIT 1
 		# 	""")
 
 		# get statistics
@@ -31,23 +40,28 @@ def main():
 
 		# get health score inputs
 		cur.execute("""
-			SELECT writesGt32msPct,writesGt64msPct,writesGt128msPct,writesGt256msPct,writesGt512msPct,writesGt1024msPct,writesGt2048msPct,writesGt4096msPct,writes0_062msPct,writes0_125msPct,writes0_25msPct,writes0_5msPct,writes1msPct,writes2msPct,writes4msPct,writes8msPct,writes16msPct,writes32msPct,writes64msPct,writes128msPct,writes256msPct,writes512msPct,writes1024msPct,writes2048msPct,writes4096msPct,writes8192msPct,writes16384msPct,writes32768msPct,writes65536msPct,reads0_062msPct,reads0_125msPct,reads0_25msPct,reads0_5msPct,reads1msPct,reads2msPct,reads4msPct,reads8msPct,reads16msPct,reads32msPct,reads64msPct,reads128msPct,reads256msPct,reads512msPct,reads1024msPct,reads2048msPct,reads4096msPct,reads8192msPct,reads16384msPct,reads32768msPct,reads65536msPct,cpuLatestTotalAvgPct,portTotalBandwidthMBPS,delAcksPct
+			SELECT systemid,"from",writes0_062msPct,writes0_125msPct,writes0_25msPct,writes0_5msPct,writes1msPct,writes2msPct,writes4msPct,writes8msPct,writes16msPct,writes32msPct,writes64msPct,writes128msPct,writes256msPct,writes512msPct,writes1024msPct,writes2048msPct,writes4096msPct,writes8192msPct,writes16384msPct,writes32768msPct,writes65536msPct,reads0_062msPct,reads0_125msPct,reads0_25msPct,reads0_5msPct,reads1msPct,reads2msPct,reads4msPct,reads8msPct,reads16msPct,reads32msPct,reads64msPct,reads128msPct,reads256msPct,reads512msPct,reads1024msPct,reads2048msPct,reads4096msPct,reads8192msPct,reads16384msPct,reads32768msPct,reads65536msPct,cpuLatestTotalAvgPct,portTotalBandwidthMBPS,delAcksPct
 			FROM hp
-			WHERE systemid = 4
-			LIMIT 10
+			LIMIT 1
 			""")
 
 		# print the results of the query	
 		rows = cur.fetchall()
-		for row in rows:
-			print("   ", row)
+		# for row in rows:
+		# 	print("   ", row)
 
+		row = rows[0]
+		# print(row)
+		systemid, timestamp, writes, reads, cpu, bandwidth, delayedAcks = parse_health_inputs_row(row)
+		print("systemid: {}\ntimestamp: {}\nwrites: {}\nreads: {}\ncpu: {}\nbandwidth: {}\ndelayedAcks: {}".format(systemid, timestamp, writes, reads, cpu, bandwidth, delayedAcks))
 
 
 		cur.close()
 		conn.close()
+	except psycopg2.ProgrammingError as e:
+		print(e)
 	except:
-		print("Unable to connect to the database")
+		print("Error connecting to database: {}".format(sys.exc_info()[0]))
 
 if __name__ == "__main__":
 	main()
