b324766cc981fc74d93cecd2743147eb63ee1a3b
sassssafraaaas <sallyli@1x-nat-vl930-172-30-109-84.wireless.umass.edu>
2016-12-10 21:06:27 -0500

    algorithm setup, pulling from CSV eventually

--- src/health_score.py
+++ src/health_score.py
@@ -5,29 +5,43 @@
 import numpy as np
 from query_db import get_test_health_inputs_row, parse_health_inputs_row
 
-
+"""
+Let c be a 21-dimensional vector defined as <2121, 2021, 1921, 1821, ... 321, 221, 121>.
+Create a 21-dimensional vector w from the fields of the form writes*msPct ordered from smallest to greatest time.
+Create a 21-dimensional vector r from the fields of the form reads*msPct ordered from smallest to greatest time.
+Let ws=wc and rs=rc. We call ws and rs the write score and read score respectively.
+Compute cbs=dist((cpu, nbw), (1, 0))/sqrt(2)=sqrt((1-cpu)^2+nbw^2)/2) where cpu corresponds to cpuLatestTotalAvgPct and nbw corresponds to portTotalBandwidthMBPS normalized by the maximum value of portTotalBandwidthMBPS seen for the particular system at hand. We call this the CPU-bandwidth score. If nbw would be undefined, we use nbw=1 in computing cbs.
+Compute the interim health score ihs=<ws, rs, cbs, 1-da><0.45, 0.25, 0.2, 0.1> on the range [0, 1] where da is the delAckPct.
+The resulting health score is floor(800*ihs) yielding a value on the range [0, 800].
+"""
+weights = [0.45,0.25,0.2,0.1]
+c = [21/21, 20/21, 19/21, 18/21. 17/21, 16/21, 15/21, 14/21, 13/21, 12/21, 11/21, 10/21, 9/21, 8/21, 7/21, 6/21, 5/21, 4/21, 3/21, 2/21, 1/21]
 write_weights = []
 read_weights = []
 
 
 def compute_health_score(ihs):
-	pass
+	readScore = compute_rs(read_weights)
+	writeScore = compute_ws(read_weights)
+	cbs = compute_cbs(cpu, bw)
+	ihs = (writeScore, readScore, cbs, daPct)
+	return 800*ihs
 
 
 def compute_ihs(ws, res, cps, daPct):
-	pass
+	return numpy.dot([ws,res, cps, 1-daPct], weights)
 
 
 def compute_cbs(cpuPct, bwPct):
-	pass
+	return numpy.sqrt(((1-cpuPct)^2+bwPct^2)/2)
 
 
 def compute_rs(readScores):
-	pass
+	return numpy.dot(readScores, c)
 
 
 def compute_ws(writeScores):
-	pass
+	return numpy.dot(writeScores, c)
 
 
 # main function for the purpose of testing
