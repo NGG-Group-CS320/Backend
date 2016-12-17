9893da4fae2344c0c5ec9094fd6e95044a47f2b8
AlexLamson <alex.lamson@gmail.com>
2016-12-16 21:25:28 -0500

    added from and to fields to health_score.py

--- src/health_score.py
+++ src/health_score.py
@@ -87,12 +87,13 @@ def get_test_health_inputs_row():
 		cur = conn.cursor()
 		# get health score inputs
 		cur.execute("""
-			SELECT systemid,"from",writes0_062msPct,writes0_125msPct,writes0_25msPct,writes0_5msPct,writes1msPct,writes2msPct,writes4msPct,writes8msPct,writes16msPct,writes32msPct,writes64msPct,writes128msPct,writes256msPct,writes512msPct,writes1024msPct,writes2048msPct,writes4096msPct,writes8192msPct,writes16384msPct,writes32768msPct,writes65536msPct,reads0_062msPct,reads0_125msPct,reads0_25msPct,reads0_5msPct,reads1msPct,reads2msPct,reads4msPct,reads8msPct,reads16msPct,reads32msPct,reads64msPct,reads128msPct,reads256msPct,reads512msPct,reads1024msPct,reads2048msPct,reads4096msPct,reads8192msPct,reads16384msPct,reads32768msPct,reads65536msPct,cpuLatestTotalAvgPct,portTotalBandwidthMBPS,delAcksPct
+			SELECT writes0_062msPct,writes0_125msPct,writes0_25msPct,writes0_5msPct,writes1msPct,writes2msPct,writes4msPct,writes8msPct,writes16msPct,writes32msPct,writes64msPct,writes128msPct,writes256msPct,writes512msPct,writes1024msPct,writes2048msPct,writes4096msPct,writes8192msPct,writes16384msPct,writes32768msPct,writes65536msPct,reads0_062msPct,reads0_125msPct,reads0_25msPct,reads0_5msPct,reads1msPct,reads2msPct,reads4msPct,reads8msPct,reads16msPct,reads32msPct,reads64msPct,reads128msPct,reads256msPct,reads512msPct,reads1024msPct,reads2048msPct,reads4096msPct,reads8192msPct,reads16384msPct,reads32768msPct,reads65536msPct,systemid,"from","to",cpuLatestTotalAvgPct,portTotalBandwidthMBPS,delAcksPct
 			FROM hp
 			LIMIT 1
 			""")
 
 		rows = cur.fetchall()
+		cur.close()
 		row = rows[0]
 		return row
 
@@ -100,7 +101,7 @@ def get_test_health_inputs_row():
 # parses query results into variables to be used to compute health score
 def parse_health_inputs_row(row):
 	# extract the fields from the row
-	systemid, timestamp, writes, reads, cpu, bandwidth, delayedAcks = row[0], row[1], row[2:2+21], row[2+21:2+21+21], row[-3], row[-2], row[-1]
+	writes, reads, systemid, from_time, to_time, cpu, bandwidth, delayedAcks = row[0:21], row[21:21+21], row[-6], row[-5], row[-4], row[-3], row[-2], row[-1]
 	writes = [float(x) for x in writes]
 	reads = [float(x) for x in reads]
 	cpu, bandwidth, delayedAcks = float(cpu), float(bandwidth), float(delayedAcks)
@@ -110,16 +111,23 @@ def parse_health_inputs_row(row):
 	writes = np.array(writes) / 100
 	cpu, bandwidth, delayedAcks = cpu/100, bandwidth/100, delayedAcks/100
 
-	return systemid, timestamp, writes, reads, cpu, bandwidth, delayedAcks
+	return systemid, from_time, to_time, writes, reads, cpu, bandwidth, delayedAcks
 
 
 # main function for the purpose of testing
 def main():
 	row = get_test_health_inputs_row() #note: this function will have to be removed at some point
-	systemid, timestamp, writes, reads, cpu, bandwidth, delayedAcks = parse_health_inputs_row(row)
-	# print("systemid: {}\ntimestamp: {}\nwrites: {}\nreads: {}\ncpu: {}\nbandwidth: {}\ndelayedAcks: {}".format(systemid, timestamp, writes, reads, cpu, bandwidth, delayedAcks))
-	score = compute_health_score(writes, reads, cpu, bandwidth, delayedAcks)
-	print("This system has a health score of {}/800 (higher is healthier)".format(score))
+	systemid, from_time, to_time, writes, reads, cpu, bandwidth, delayedAcks = parse_health_inputs_row(row)
+	
+	print(type(from_time))
+	print(from_time)
+	unixtime = time.mktime(d.timetuple())
+
+	# print("systemid: {}\nfrom_time: {}\nto_time: {}\nwrites: {}\nreads: {}\ncpu: {}\nbandwidth: {}\ndelayedAcks".format(systemid, from_time, to_time, writes, reads, cpu, bandwidth, delayedAcks))
+	# print("systemid: {}\nfrom_time: {}\nto_time: {}\nwrites: {}\nreads: {}\ncpu: {}\nbandwidth: {}\ndelayedAcks".format(systemid, from_time, to_time, np.sum(writes), np.sum(reads), cpu, bandwidth, delayedAcks))
+
+	# score = compute_health_score(writes, reads, cpu, bandwidth, delayedAcks)
+	# print("This system has a health score of {}/800 (higher is healthier)".format(score))
 
 if __name__ == "__main__":
 	main()
